5a3351ed53bf5f1a2a49233a2eb6e16e0fc8e532
Juan Martinez <111025032+jmartnez@users.noreply.github.com>
2023-05-15 01:38:33 -0300

    Subiendo cambios al proyecto.

---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { NuevoUsuario } from '../model/nuevo-usuario';
+import { LoginUsuario } from '../model/login-usuario';
+import { Jwtdto } from '../model/jwtdto';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  authURL='https://localhost:8080/auth/';
+
+  constructor(private httpClient: HttpClient) { }
+
+
+  public nuevo(nuevoUsuario: NuevoUsuario): Observable<any>{
+    return this.httpClient.post<any>(this.authURL + 'nuevo', nuevoUsuario);
+  }
+
+  public login(loginUsuario: LoginUsuario):Observable<Jwtdto>{
+    return this.httpClient.post<Jwtdto>(this.authURL + 'login', loginUsuario)
+  }
+}
